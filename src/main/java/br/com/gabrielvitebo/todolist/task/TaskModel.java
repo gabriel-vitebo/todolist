1801a5a0dbed1e17a76e83bf472edda397e967ac
gabriel-vitebo <vitebo@outlook.com>
2023-10-11 16:33:47 -0300

    create table task

---
+++ src/main/java/br/com/gabrielvitebo/todolist/task/TaskModel.java
@@ -0,0 +1,34 @@
+package br.com.gabrielvitebo.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private UUID user_id;
+  private String description;
+
+  @Column(length = 50)
+  private String title;
+  private LocalDateTime start_at;
+  private LocalDateTime endt_at;
+  private String priority;
+
+  @CreationTimestamp
+  private LocalDateTime created_at;
+
+
+}
